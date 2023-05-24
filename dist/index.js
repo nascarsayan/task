df7b30ec748db41944e5fa0ecd3ea74bad736eeb
Sayan Naskar <nascarsayan@gmail.com>
2023-05-24 10:21:26 +0530

    GET /tasks

--- dist/index.js
+++ dist/index.js
@@ -6,10 +6,25 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // const express = require('express');
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-function handleAll(req, res) {
-    return res.send('Hello World!');
+function handleRoot(req, res) {
+    return res.send("Hello World!");
 }
-app.get('/', handleAll);
+app.get("/", handleRoot);
+app.get("/tasks", (req, res) => {
+    const tasks = [
+        {
+            id: "1",
+            description: "Drink water",
+            completed: false,
+        },
+        {
+            id: "2",
+            description: "Eat food",
+            completed: true,
+        }
+    ];
+    res.send(tasks);
+});
 app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+    console.log("Server is running on http://localhost:3000");
 });
--- index.ts
+++ index.ts
@@ -1,17 +1,30 @@
 // const express = require('express');
-import express, { Express, Request, Response } from 'express';
-
+import express, { Express, Request, Response } from "express";
 
 const app = express();
 
-function handleAll(req: Request, res: Response) {
-    return res.send('Hello World!');
+function handleRoot(req: Request, res: Response) {
+  return res.send("Hello World!");
 }
 
-app.get('/', handleAll);
+app.get("/", handleRoot);
+
+app.get("/tasks", (req: Request, res: Response) => {
+  const tasks = [
+    {
+      id: "1",
+      description: "Drink water",
+      completed: false,
+    },
+    {
+      id: "2",
+      description: "Eat food",
+      completed: true,
+    }
+  ];
+  res.send(tasks);
+});
 
 app.listen(3000, () => {
-    console.log(
-        'Server is running on http://localhost:3000'
-    );
+  console.log("Server is running on http://localhost:3000");
 });
