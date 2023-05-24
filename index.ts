10e29749881d5b691cefbf79f7e08dbeed88dbb1
Sayan Naskar <nascarsayan@gmail.com>
2023-05-24 12:46:47 +0530

    POST request working

--- index.ts
+++ index.ts
@@ -30,8 +30,49 @@ app.get("/tasks", (req: Request, res: Response) => {
 app.post("/tasks", (req: Request, res: Response) => {
   // extract the new task from the request body.
   const newTask = req.body;
+  // first check if all the required properties are sent in the request body.
+  if (!(
+    newTask.id &&
+    typeof newTask.id === "string")) {
+    return res
+      .status(400)
+      .send("id is required and should be a string");
+  }
+  if (!(
+    newTask.description &&
+    typeof newTask.description === "string")) {
+    return res
+      .status(400)
+      .send("description is required and should be a string");
+  }
+  if (!(newTask.completed !== undefined && typeof newTask.completed === "boolean")) {
+    return res
+      .status(400)
+      .send("completed is required and should be a boolean");
+  }
+  for (const task of tasks) {
+    if (task.id === newTask.id) {
+      return res
+        .status(400)
+        .send("Another task with the same id already exists");
+    }
+  }
+  // Another check could be that only these three properties are sent in the request body. Any other property should not be allowed.
+  // extract the properties of the new task.
+  const {
+    id,
+    description,
+    completed,
+  } = newTask;
+  // add the new task to the tasks array.
+  const t = {
+    id,
+    description,
+    completed,
+  }
+  tasks.push(t);
   console.log(newTask);
-  res.send("POST request received");
+  res.send(t);
 })
 
 app.listen(4000, () => {
