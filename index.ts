7abf03a2e1c683ccff1f1336987a1d12c480aabf
Sayan Naskar <nascarsayan@gmail.com>
2023-05-24 09:47:50 +0530

    db.json

--- index.ts
+++ index.ts
@@ -1,17 +1,28 @@
 // const express = require('express');
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response } from "express";
 
+import data from "./data/db.json";
+
+interface Task {
+  id: string;
+  description: string;
+  completed: boolean;
+}
+
+const tasks: Task[] = data.tasks;
 
 const app = express();
 
 function handleAll(req: Request, res: Response) {
-    return res.send('Hello World!');
+  return res.send("Hello World!");
 }
 
-app.get('/', handleAll);
+app.get("/", handleAll);
+
+app.get("/tasks", (req: Request, res: Response) => {
+  return res.json(tasks);
+});
 
 app.listen(3000, () => {
-    console.log(
-        'Server is running on http://localhost:3000'
-    );
+  console.log("Server is running on http://localhost:3000");
 });
